c8feae7c1e1f04b43564fbe90133de28a375f39d
Alex Dauncey <alexanderdauncey@gmail.com>
2022-08-18 19:43:46 +0100

    Print errors to std err instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem Parsing Arguments: {err}");
+        eprintln!("Problem Parsing Arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
 
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
 
         process::exit(1);
     }
